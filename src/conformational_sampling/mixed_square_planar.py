c9a1b206dc39104ba95b81835b54831871bc8842
joshkamm <joshkamm@umich.edu>
2022-06-03 04:45:13 -0400

    wip #1 code organization, exploring parallelism

---
+++ src/conformational_sampling/mixed_square_planar.py
@@ -0,0 +1,42 @@
+from stk.molecular.topology_graphs import Edge
+from stk.molecular.topology_graphs.metal_complex import MetalComplex
+from stk.molecular.topology_graphs.metal_complex.vertices import BiDentateLigandVertex, MonoDentateLigandVertex, MetalVertex
+
+class MixedSquarePlanar(MetalComplex):
+
+    _metal_vertex_prototypes = (
+        MetalVertex(0, (0, 0, 0)),
+    )
+    _ligand_vertex_prototypes = (
+        BiDentateLigandVertex(1, (2.5, 2.5, 0)),
+        MonoDentateLigandVertex(2, (-2.5, 0, 0)),
+        MonoDentateLigandVertex(3, (0, -2.5, 0)),
+    )
+    
+    _edge_prototypes = (
+        Edge(
+            id=0,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[0],
+            position=(2.5, 0, 0),
+        ),
+        Edge(
+            id=1,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[0],
+            position=(0, 2.5, 0),
+        ),
+
+        Edge(
+            id=2,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[1],
+            position=(-1, 0, 0),
+        ),
+        Edge(
+            id=3,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[2],
+            position=(0, -1, 0),
+        ),
+    )
--- src/conformational_sampling/test.py
+++ src/conformational_sampling/test.py
@@ -1,48 +1,4 @@
 # %%
-from stk.molecular.topology_graphs import Edge
-from stk.molecular.topology_graphs.metal_complex import MetalComplex
-from stk.molecular.topology_graphs.metal_complex.vertices import BiDentateLigandVertex, MonoDentateLigandVertex, MetalVertex
-
-class CustomSquarePlanar(MetalComplex):
-
-    _metal_vertex_prototypes = (
-        MetalVertex(0, (0, 0, 0)),
-    )
-    _ligand_vertex_prototypes = (
-        BiDentateLigandVertex(1, (2.5, 2.5, 0)),
-        MonoDentateLigandVertex(2, (-2.5, 0, 0)),
-        MonoDentateLigandVertex(3, (0, -2.5, 0)),
-    )
-    
-    _edge_prototypes = (
-        Edge(
-            id=0,
-            vertex1=_metal_vertex_prototypes[0],
-            vertex2=_ligand_vertex_prototypes[0],
-            position=(2.5, 0, 0),
-        ),
-        Edge(
-            id=1,
-            vertex1=_metal_vertex_prototypes[0],
-            vertex2=_ligand_vertex_prototypes[0],
-            position=(0, 2.5, 0),
-        ),
-
-        Edge(
-            id=2,
-            vertex1=_metal_vertex_prototypes[0],
-            vertex2=_ligand_vertex_prototypes[1],
-            position=(-1, 0, 0),
-        ),
-        Edge(
-            id=3,
-            vertex1=_metal_vertex_prototypes[0],
-            vertex2=_ligand_vertex_prototypes[2],
-            position=(0, -1, 0),
-        ),
-    )
-
-# %%
 %reload_ext autoreload
 %autoreload 2
 from IPython.display import display
@@ -53,13 +9,12 @@ import openbabel as ob
 from openbabel import pybel as pb
 import nglview
 
+from conformational_sampling.mixed_square_planar import MixedSquarePlanar
+
 vitek_dmpp_ligand = Path('/export/zimmerman/joshkamm/Lilly/py-conformational-sampling/examples/vitek_dmpp/ligand.xyz')
-target = Path('/export/zimmerman/joshkamm/Lilly/py-conformational-sampling/examples/vitek_dmpp/target.xyz')
+# vitek_dmpp_ligand = Path('/export/zimmerman/joshkamm/Lilly/py-conformational-sampling/examples/bis_hydrazone/bis_hydrazone_ligand.xyz')
 vitek_dmpp_ligand = next(pb.readfile('xyz', str(vitek_dmpp_ligand)))
-target = next(pb.readfile('xyz', str(target)))
 vitek_dmpp_ligand = MolFromMolBlock(vitek_dmpp_ligand.write('mol'), removeHs=False)
-target = MolFromMolBlock(target.write('mol'), removeHs=False)
-print(MolToMolBlock(target))
 nglview.show_rdkit(vitek_dmpp_ligand)
 
 
@@ -68,56 +23,69 @@ from rdkit import Chem
 import stk
 import stko
 
-metal = stk.BuildingBlock(
-    smiles='[Pd]',
-    functional_groups=(
-        stk.SingleAtom(stk.Pd(0))
-        for i in range(6)
-    ),
-    position_matrix=[[0, 0, 0]],
-)
+def bind_to_dimethyl_Pd(ligand):
+    metal = stk.BuildingBlock(
+        smiles='[Pd]',
+        functional_groups=(
+            stk.SingleAtom(stk.Pd(0))
+            for i in range(6)
+        ),
+        position_matrix=[[0, 0, 0]],
+    )
 
-methyl = stk.BuildingBlock(
-    smiles='[CH3]',
-    functional_groups=[
-        stk.SingleAtom(stk.C(0))
-    ]
-)
+    methyl = stk.BuildingBlock(
+        smiles='[CH3]',
+        functional_groups=[
+            stk.SingleAtom(stk.C(0))
+        ]
+    )
 
-Chem.rdmolops.Kekulize(vitek_dmpp_ligand)
-vitek_dmpp_ligand_stk = stk.BuildingBlock.init_from_rdkit_mol(
-    vitek_dmpp_ligand,
-    functional_groups=[
-        stk.SmartsFunctionalGroupFactory(
-            smarts='P',
-            bonders=(0,),
-            deleters=(),
-        )
-    ]
-)
+    Chem.rdmolops.Kekulize(ligand)
+    ligand_stk = stk.BuildingBlock.init_from_rdkit_mol(
+        ligand,
+        functional_groups=[
+            stk.SmartsFunctionalGroupFactory(
+                smarts='P',
+                bonders=(0,),
+                deleters=(),
+            )
+        ]
+    )
 
-complex = stk.ConstructedMolecule(
-    topology_graph=CustomSquarePlanar(
-        metals=metal,
-        ligands={
-            vitek_dmpp_ligand_stk: (0, ),
-            methyl: (1, 2),
-        },
-    ),
-)
+    return stk.ConstructedMolecule(
+        topology_graph=MixedSquarePlanar(
+            metals=metal,
+            ligands={
+                ligand_stk: (0, ),
+                methyl: (1, 2),
+            },
+        ),
+    )
 
+complex = bind_to_dimethyl_Pd(vitek_dmpp_ligand)
 mol = complex.to_rdkit_mol()
 Chem.SanitizeMol(mol)
-display(nglview.show_rdkit(mol))
+# display(nglview.show_rdkit(mol))
 complex = stk.MCHammer().optimize(complex)
 mol = complex.to_rdkit_mol()
 Chem.SanitizeMol(mol)
-display(nglview.show_rdkit(mol))
+# display(nglview.show_rdkit(mol))
 complex = stko.MetalOptimizer().optimize(complex)
 mol = complex.to_rdkit_mol()
 Chem.SanitizeMol(mol)
 display(nglview.show_rdkit(mol))
-complex = stko.XTB('/export/apps/CentOS7/xtb/xtb/bin/xtb').optimize(complex)
-mol = complex.to_rdkit_mol()
+complexes = [complex] * 2
+from concurrent.futures import ProcessPoolExecutor
+def execute_xtb(idx, complex):
+    return stko.XTB(
+        '/export/apps/CentOS7/xtb/xtb/bin/xtb',
+        output_dir=f'xtb_test_{idx}',
+        calculate_hessian=False,
+        max_runs=1,
+        charge=0
+    ).optimize(complex)
+with ProcessPoolExecutor(max_workers=2) as executor:
+    optimized_complexes = list(executor.map(execute_xtb, range(len(complexes)), complexes))
+mol = optimized_complexes[0].to_rdkit_mol()
 Chem.SanitizeMol(mol)
 display(nglview.show_rdkit(mol))
