5fca8049678c5bc27302d1eb4b4b29ab543a0ad4
joshkamm <joshkamm@umich.edu>
2022-07-04 13:29:42 -0400

    wip #1 tracked down error with functional groups

--- src/conformational_sampling/slurm.py
+++ src/conformational_sampling/slurm.py
@@ -1,3 +1,8 @@
+# %%
+# jupyter only imports
+%reload_ext autoreload
+%autoreload 2
+
 #!/export/zimmerman/joshkamm/apps/mambaforge/envs/conformational-sampling/bin/python
 #SBATCH -p zimintel --job-name=conformational_sampling
 #SBATCH -c4
@@ -11,11 +16,18 @@ mol_path = Path('ligand.xyz')
 stk_ligand = load_stk_mol(mol_path)
 
 # functional groups define which atoms of the ligand bind to the metal
-functional_groups = stk.SmartsFunctionalGroupFactory(
+# functional_groups = stk.SmartsFunctionalGroupFactory(
+#     smarts='P',
+#     bonders=(0,),
+#     deleters=(),
+# ).get_functional_groups(stk_ligand)
+# stk_ligand = stk_ligand.with_functional_groups(functional_groups)
+
+functional_group_factory = stk.SmartsFunctionalGroupFactory(
     smarts='P',
     bonders=(0,),
     deleters=(),
-).get_functional_groups(stk_ligand)
-stk_ligand = stk_ligand.with_functional_groups(functional_groups)
+)
+stk_ligand = stk.BuildingBlock.init_from_molecule(stk_ligand, functional_groups=[functional_group_factory])
 
 gen_ligand_library_entry(stk_ligand)
