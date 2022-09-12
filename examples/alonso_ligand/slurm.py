c8ed1d9bf389a7d2339c65802a8cceed7d504de9
joshkamm <joshkamm@umich.edu>
2022-09-12 12:12:34 -0400

    wip #13 added more complex alonso ligand example
    also restructured directory format of previous dppe example

---
+++ examples/alonso_ligand/slurm.py
@@ -0,0 +1,26 @@
+#!/export/zimmerman/joshkamm/apps/mambaforge/envs/conformational-sampling/bin/python
+#SBATCH -p guest --job-name=conformational_sampling
+#SBATCH -c16
+#SBATCH -o output.txt
+
+from pathlib import Path
+import stk
+from conformational_sampling.main import load_stk_mol, gen_ligand_library_entry
+from conformational_sampling.config import Config
+
+ligand_path = Path('ligand.xyz') # name of file containing ligand geometry
+stk_ligand = load_stk_mol(ligand_path)
+
+# specifies atoms of the ligand that bind to the metal, in this case as a smarts string
+functional_group_factory = stk.SmartsFunctionalGroupFactory(
+    smarts='P',
+    bonders=(0,),
+    deleters=(),
+)
+stk_ligand = stk.BuildingBlock.init_from_molecule(stk_ligand, functional_groups=[functional_group_factory])
+
+# py-conformational-sampling configuration object
+config = Config(initial_conformers=100, xtb_path='/export/apps/CentOS7/xtb/xtb/bin/xtb')
+
+# generates conformers, performs multiple step optimization and uniqueness filtering
+gen_ligand_library_entry(stk_ligand, config)
