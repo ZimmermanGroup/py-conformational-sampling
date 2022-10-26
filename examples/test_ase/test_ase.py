53bb4915fa654f15431fd6510e06efebd6d97f97
joshkamm <joshkamm@umich.edu>
2022-10-26 16:17:29 -0400

    wip #11 playing with ASE

--- examples/test_ase/test_ase.py
+++ examples/test_ase/test_ase.py
@@ -1,11 +1,13 @@
 from ase.build import molecule
 from ase.calculators.qchem import QChem
 from ase.optimize import LBFGS
+from ase.io import read, write
 import os
 from pathlib import Path
 
 os.environ['QCSCRATCH'] = 'qc_scratch'
-mol = molecule('C2H6')
+mol = read('NePF3.xyz')
+# mol = molecule('C2H6')
 calc = QChem(label='ethane',
              method='B3LYP',
              basis='6-31+G*')
