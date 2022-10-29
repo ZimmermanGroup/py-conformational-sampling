fc4aa95ff38788683e2191479af8c9b670baf7a1
joshkamm <joshkamm@umich.edu>
2022-10-29 00:44:21 -0400

    wip #11 trying to get ase trajectory to stk

--- examples/test_ase/test_ase.py
+++ examples/test_ase/test_ase.py
@@ -1,5 +1,6 @@
 from ase.build import molecule
 from ase.calculators.qchem import QChem
+from ase.calculators.emt import EMT
 from ase.optimize import LBFGS
 from ase.io import read, write
 import ase
@@ -9,24 +10,29 @@ from rdkit.Chem.rdmolfiles import MolToXYZBlock
 
 import stk
 
+from conformational_sampling.main import stk_list_to_xyz_file
+
 os.environ['QCSCRATCH'] = 'qc_scratch'
+
 stk_mol = stk.BuildingBlock(smiles='[Ne][P](F)(F)(F)')
 ase_mol = ase.Atoms(
     positions=list(stk_mol.get_atomic_positions()),
     numbers=[atom.get_atomic_number() for atom in stk_mol.get_atoms()]
 )
-write('test.xyz', ase_mol)
-# rdkit_mol = stk_mol.to_rdkit_mol()
-# MolToXYZBlock(rdkit_mol)
-# mol = read('NePF3.xyz')
-# mol = molecule('C2H6')
-calc = QChem(
-    label='NePF3',
-    method='B3LYP',
-    basis='6-31+G*',
-    nt=4
-)
+# write('test.xyz', ase_mol)
+# calc = QChem(
+#     label='NePF3',
+#     method='PBE',
+#     basis='6-31G',
+#     nt=4
+# )
+
+ase_mol = molecule('H2O')
+calc = EMT()
+
 ase_mol.calc = calc
 opt = LBFGS(ase_mol)
 opt.run()
 print(opt.s) # list 
+stk_trajectory = [stk_mol.with_position_matrix(pos_matrix) for pos_matrix in opt.s] # shapes of ndarrays don't match
+stk_list_to_xyz_file(stk_trajectory, 'test_trajectory.xyz')
