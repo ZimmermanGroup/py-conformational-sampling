a089f34e6ce20a977f8ea2ddbc266bffa571d0e1
joshkamm <joshkamm@umich.edu>
2022-11-03 02:55:29 -0400

    wip #11 accessing trajectory to debug

--- examples/test_ase/test_ase.py
+++ examples/test_ase/test_ase.py
@@ -1,9 +1,10 @@
 from ase.build import molecule
 from ase.calculators.qchem import QChem
 from ase.calculators.emt import EMT
-from ase.optimize import LBFGS
+from ase.optimize import BFGS
 from ase.io import read, write
 import ase
+from ase.io.trajectory import Trajectory
 import os
 from pathlib import Path
 from rdkit.Chem.rdmolfiles import MolToXYZBlock
@@ -15,24 +16,26 @@ from conformational_sampling.main import stk_list_to_xyz_file
 os.environ['QCSCRATCH'] = 'qc_scratch'
 
 stk_mol = stk.BuildingBlock(smiles='[Ne][P](F)(F)(F)')
+stk_mol = stk.BuildingBlock(smiles='CC')
 ase_mol = ase.Atoms(
     positions=list(stk_mol.get_atomic_positions()),
     numbers=[atom.get_atomic_number() for atom in stk_mol.get_atoms()]
 )
 # write('test.xyz', ase_mol)
-# calc = QChem(
-#     label='NePF3',
-#     method='PBE',
-#     basis='6-31G',
-#     nt=4
-# )
+calc = QChem(
+    label='ethane',
+    method='PBE',
+    basis='6-31G',
+    nt=4
+)
 
-ase_mol = molecule('H2O')
-calc = EMT()
+# ase_mol = molecule('H2O')
+# calc = EMT()
 
 ase_mol.calc = calc
-opt = LBFGS(ase_mol)
+opt = BFGS(ase_mol, trajectory='test.traj')
 opt.run()
-print(opt.s) # list 
-stk_trajectory = [stk_mol.with_position_matrix(pos_matrix) for pos_matrix in opt.s] # shapes of ndarrays don't match
+trajectory = Trajectory('test.traj')
+# print(opt.s) # list 
+stk_trajectory = [stk_mol.with_position_matrix(atoms.get_positions()) for atoms in trajectory]
 stk_list_to_xyz_file(stk_trajectory, 'test_trajectory.xyz')
