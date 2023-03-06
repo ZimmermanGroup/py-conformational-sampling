32f437e57b63e6be765a0c0d2311be89927b81a4
joshkamm <joshkamm@umich.edu>
2023-03-06 17:20:30 -0500

    wip #37 got pyGSM simple example mostly running

--- examples/gsm/gsm_example.py
+++ examples/gsm/gsm_example.py
@@ -85,12 +85,15 @@ import ase.io
 import numpy as np
 from ase.calculators.morse import MorsePotential
 
-from pygsm.coordinate_systems import DelocalizedInternalCoordinates
+from pygsm.coordinate_systems import DelocalizedInternalCoordinates, PrimitiveInternalCoordinates, Topology
 from pygsm.level_of_theories.ase import ASELoT
 from pygsm.optimizers import eigenvector_follow
 from pygsm.potential_energy_surfaces import PES
 from pygsm.utilities import elements, manage_xyz, nifty
 from pygsm.wrappers import Molecule
+from pygsm.growing_string_methods import SE_GSM
+from pygsm.wrappers.main import plot as gsm_plot
+from pygsm.wrappers.main import get_driving_coord_prim, Distance
 
 
 def main(geom):
@@ -108,24 +111,44 @@ def main(geom):
     atom_symbols = manage_xyz.get_atoms(geom)
     ELEMENT_TABLE = elements.ElementData()
     atoms = [ELEMENT_TABLE.from_symbol(atom) for atom in atom_symbols]
-    # top = Topology.build_topology(
-    #     xyz,
-    #     atoms,
-    # )
+    top = Topology.build_topology(
+        xyz,
+        atoms,
+        add_bond=[(4,12),(1,11)],
+    )
 
-    # nifty.printcool("Building Primitive Internal Coordinates")
-    # p1 = PrimitiveInternalCoordinates.from_options(
-    #     xyz=xyz,
-    #     atoms=atoms,
-    #     addtr=False,  # Add TRIC
-    #     topology=top,
-    # )
+    driving_coordinates = [['ADD',4,12],['ADD',1,11]]
+    driving_coord_prims = []
+    for dc in driving_coordinates:
+        prim = get_driving_coord_prim(dc)
+        if prim is not None:
+            driving_coord_prims.append(prim)
+
+    for prim in driving_coord_prims:
+        if type(prim) == Distance:
+            bond = (prim.atoms[0], prim.atoms[1])
+            if bond in top.edges:
+                pass
+            elif (bond[1], bond[0]) in top.edges():
+                pass
+            else:
+                print(" Adding bond {} to top1".format(bond))
+                top.add_edge(bond[0], bond[1])
+
+    nifty.printcool("Building Primitive Internal Coordinates")
+    p1 = PrimitiveInternalCoordinates.from_options(
+        xyz=xyz,
+        atoms=atoms,
+        addtr=False,  # Add TRIC
+        topology=top,
+    )
 
     nifty.printcool("Building Delocalized Internal Coordinates")
     coord_obj1 = DelocalizedInternalCoordinates.from_options(
         xyz=xyz,
         atoms=atoms,
         addtr=False,  # Add TRIC
+        primitives=p1,
     )
 
     nifty.printcool("Building Molecule")
@@ -141,16 +164,26 @@ def main(geom):
                                                 conv_Ediff=0.5)
 
     nifty.printcool("initial energy is {:5.4f} kcal/mol".format(reactant.energy))
-    geoms, energies = optimizer.optimize(
-        molecule=reactant,
-        refE=reactant.energy,
-        opt_steps=5,
-        verbose=True,
+    # geoms, energies = optimizer.optimize(
+    #     molecule=reactant,
+    #     refE=reactant.energy,
+    #     opt_steps=5,
+    #     verbose=True,
+    # )
+
+    # nifty.printcool("Final energy is {:5.4f}".format(reactant.energy))
+    # manage_xyz.write_xyz('minimized.xyz', geoms[-1], energies[-1], scale=1.)
+    
+    # trying out GSM
+    se_gsm = SE_GSM.from_options(
+        reactant=reactant,
+        nnodes=7,
+        optimizer=optimizer,
+        driving_coords=[['ADD',4,12],['ADD',1,11]],        
     )
-
-    nifty.printcool("Final energy is {:5.4f}".format(reactant.energy))
-    manage_xyz.write_xyz('minimized.xyz', geoms[-1], energies[-1], scale=1.)
-
+    
+    se_gsm.go_gsm()
+    gsm_plot(se_gsm.energies, x=range(len(se_gsm.energies)), title=0)
 
 if __name__ == '__main__':
     diels_adler = ase.io.read("/export/zimmerman/joshkamm/Lilly/pyGSM/data/diels_alder.xyz", ":")
