0025c672de0c213c888bfd5c9b30b31659508ae3
joshkamm <joshkamm@umich.edu>
2023-03-16 04:09:42 -0400

    wip #36 test stk to pybel loop

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,15 @@
-import conformational_sampling
+import numpy as np
+import stk
+from conformational_sampling.main import pybel_mol_to_stk_mol, stk_mol_to_pybel_mol
 
 def test_imports():
     assert True
+
+def test_stk_to_pybel_loop():
+    'convert an stk molecule to a pybel molecule and then back to validate the conversion methods'
+    butane_initial = stk.BuildingBlock('CCCC')
+    butane_final = pybel_mol_to_stk_mol(stk_mol_to_pybel_mol(butane_initial))
+    initial_positions = butane_initial.get_position_matrix()
+    final_positions = butane_final.get_position_matrix()
+    assert np.allclose(initial_positions, final_positions, atol=1e-3)
+                                        
