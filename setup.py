53d9bf2e748ab1b9f82dfbfff3b82fb0ea007421
joshkamm <joshkamm@umich.edu>
2023-04-25 01:39:31 -0400

    #37 increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 setuptools.setup(
     name="py-conformational-sampling",
-    version="0.2.1",
+    version="0.2.2",
     # description="",
     long_description=README,
     long_description_content_type="text/markdown",
