761087d9aeaf91204750928898a8219a0e365f2b
joshkamm <joshkamm@umich.edu>
2023-04-25 01:36:25 -0400

    wip #37 clean up test_gsm.py

--- tests/test_gsm.py
+++ tests/test_gsm.py
@@ -1,15 +1,15 @@
 from pathlib import Path
-import subprocess as sp
-import pytest
-from xtb.ase.calculator import XTB
 
+import pytest
 import stk
 import stko
+from xtb.ase.calculator import XTB
+
 from conformational_sampling.ase_stko_optimizer import ASE
 from conformational_sampling.config import Config
 from conformational_sampling.gsm import stk_gsm
+from conformational_sampling.main import (bind_to_dimethyl_Pd, load_stk_mol)
 
-from conformational_sampling.main import bind_to_dimethyl_Pd, load_stk_mol, stk_list_to_xyz_file, xtb_optimize
 
 @pytest.mark.xfail(raises=RuntimeError)
 def test_gsm():
@@ -25,35 +25,19 @@ def test_gsm():
     )
     stk_ligand = stk.BuildingBlock.init_from_molecule(stk_ligand, functional_groups=[functional_group_factory])
     stk_mol = bind_to_dimethyl_Pd(stk_ligand)
-    stk_list_to_xyz_file([stk_mol], Path('test.xyz'))
     optimizer_sequence = stko.OptimizerSequence(stk.MCHammer(), stko.MetalOptimizer(), ASE(XTB()))
     stk_mol = optimizer_sequence.optimize(stk_mol)
     
-    # result = sp.run(
-    #             'which xtb',
-    #             stdout=sp.PIPE,
-    #             shell=True
-    #         )
-    # xtb_path = result.stdout.decode('UTF-8')[:-1]
-    # print(xtb_path)
-    # stk_mol = xtb_optimize(0, stk_mol, xtb_path='/export/apps/CentOS7/xtb/xtb/bin/xtb')
-    # stk_mol = xtb_optimize(0, stk_mol, xtb_path=xtb_path)
-    
-    stk_list_to_xyz_file([stk_mol], Path('test.xyz'))
-
     # run gsm to eliminate ethane
     # driving coordinates are 1-indexed
     driving_coordinates = [['BREAK',1,54],['BREAK',1,58],['ADD',54,58]]
     config = Config(
         xtb_path='/export/apps/CentOS7/xtb/xtb/bin/xtb',
-        # ase_calculator=XTB(method='GFN-FF'),
         ase_calculator=XTB(),
     )
-    # with pytest.raises(RuntimeError, ma) as excinfo:
+    
     stk_gsm(
         stk_mol=stk_mol,
         driving_coordinates=driving_coordinates,
         config=config,
     )
-    # assert str(excinfo) == "TS node shouldn't be the first or last node"        
-
