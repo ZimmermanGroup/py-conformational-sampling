7d682bbcf46f79458d74443721c93de171639d07
joshkamm <joshkamm@umich.edu>
2023-06-22 14:38:42 -0400

    wip #21 interactive visualization of TS's
    also refactored analyze.py into function

--- src/conformational_sampling/analyze.py
+++ src/conformational_sampling/analyze.py
@@ -12,34 +12,6 @@ from rdkit.Chem import rdMolTransforms
 from conformational_sampling.utils import pybel_mol_to_rdkit_mol
 
 
-path = Path('scratch')
-# path = Path('/export/zimmerman/soumikd/py-conformational-sampling/scratch')
-file_list = []
-
-for name in os.listdir(path):
-    if name.startswith("pystring"):
-        #dir_list.append(names)
-        in_path = path / f'{name}'
-        if 'opt_converged_000.xyz' in os.listdir(in_path):
-            file_list.append(str(in_path)+"/opt_converged_000.xyz")
-        # new_path = Path(f'scratch/{names}/scratch')
-        # for name in os.listdir(new_path):
-        #     if name.startswith("pystring"):
-        #         in_in_path = Path(f'scratch/{names}/scratch/{name}')
-        #         if 'opt_converged_000.xyz' in os.listdir(in_in_path):
-        #             file_list.append(str(in_in_path)+"/opt_converged_000.xyz")
-
-# print(sorted(file_list))
-
-infofile = open('TS_and_stereo.txt', 'w')
-infofile.write("   Conformers      TS_Energy_Barrier     Stereochemistry   Torsion angle (deg)\n")
-
-ensemble_TS = open('ensemble_TS.xyz', 'w')
-
-# Conformer Stereochemistry: Dihedral C(OMe)-C(Ph)-C(Ph)-C(Me)
-# -180 to 0 --> R and 0 to 180 --> S
-
-
 # Function to determine the existence and position of a unique TS node along the string
  
 def ts_node(en_list):
@@ -59,92 +31,121 @@ def ts_node(en_list):
     return (max_diff, ts_node_energy, ts_barrier)
 
 
-for file in file_list:
-    with open(file, 'r') as f:
-        flines = f.read().splitlines()
+def analyze():
+    path = Path('scratch')
+    # path = Path('/export/zimmerman/soumikd/py-conformational-sampling/scratch')
+    file_list = []
 
-        try:
-            natoms = int(flines[0])
-            nlines = natoms + 2
+    for name in os.listdir(path):
+        if name.startswith("pystring"):
+            #dir_list.append(names)
+            in_path = path / f'{name}'
+            if 'opt_converged_000.xyz' in os.listdir(in_path):
+                file_list.append(str(in_path)+"/opt_converged_000.xyz")
+            # new_path = Path(f'scratch/{names}/scratch')
+            # for name in os.listdir(new_path):
+            #     if name.startswith("pystring"):
+            #         in_in_path = Path(f'scratch/{names}/scratch/{name}')
+            #         if 'opt_converged_000.xyz' in os.listdir(in_in_path):
+            #             file_list.append(str(in_in_path)+"/opt_converged_000.xyz")
 
-            line = 1
-            en_list = []
-            while line < len(flines):
-                en_list.append(float(flines[line]))
-                line += nlines
-        
-        except:
-            natoms = int(flines[2])
-            nlines = natoms + 2
+    # print(sorted(file_list))
 
-            start_index = flines.index('energy')
-            end_index = flines.index('max-force')
+    infofile = open('TS_and_stereo.txt', 'w')
+    infofile.write("   Conformers      TS_Energy_Barrier     Stereochemistry   Torsion angle (deg)\n")
 
-            en_list = []
+    ensemble_TS = open('ensemble_TS.xyz', 'w')
 
-            for i in range(start_index+1, end_index):
-                en_list.append(float(flines[i]))
+    # Conformer Stereochemistry: Dihedral C(OMe)-C(Ph)-C(Ph)-C(Me)
+    # -180 to 0 --> R and 0 to 180 --> S
 
-            with open('file_xyz.xyz', 'w') as f_xyz:
-                f_xyz.write('\n'.join(flines[2:start_index-1]))    
-        
-        if ts_node(en_list)[0] != 0.0:
+
+
+
+    for file in file_list:
+        with open(file, 'r') as f:
+            flines = f.read().splitlines()
 
             try:
-                pybel_product = list(pb.readfile(format='xyz', filename='file_xyz.xyz'))[-1]
-                TS_index = en_list.index(ts_node(en_list)[1])*nlines + 2
-            except:
-                pybel_product = list(pb.readfile(format='xyz', filename=file))[-1]
-                TS_index = en_list.index(ts_node(en_list)[1])*nlines
+                natoms = int(flines[0])
+                nlines = natoms + 2
 
-            rdkit_product = pybel_mol_to_rdkit_mol(pybel_product)
-            torsion_deg = rdMolTransforms.GetDihedralDeg(rdkit_product.GetConformer(), 56, 55, 79, 78)
-            if torsion_deg >= 0:
-                stereochem = 'S'
-            else:
-                stereochem = 'R'
-            # 
-            idx = re.search(r"pystring_(\d+)", file).groups()[0]
+                line = 1
+                en_list = []
+                while line < len(flines):
+                    en_list.append(float(flines[line]))
+                    line += nlines
             
-            infofile.write(f"  Conformer_{idx}     {ts_node(en_list)[2]:.6f}      {stereochem}           {torsion_deg:.3f}\n")
+            except:
+                natoms = int(flines[2])
+                nlines = natoms + 2
 
-            ensemble_TS.write('\n'.join(flines[TS_index:TS_index+nlines]))
-            ensemble_TS.write('\n')
+                start_index = flines.index('energy')
+                end_index = flines.index('max-force')
 
-ensemble_TS.close()
-try:
-    os.remove('file_xyz.xyz')
-except:
-    pass
-    
-## Script for submitting the TS jobs
-## subprocess.run(['sbatch', f'--array=0-{len(file_list)-1}', './tests/ts_job_array.py'])
+                en_list = []
 
-## Script for making the TS job input files
+                for i in range(start_index+1, end_index):
+                    en_list.append(float(flines[i]))
 
-new_path = Path('OptTS')
+                with open('file_xyz.xyz', 'w') as f_xyz:
+                    f_xyz.write('\n'.join(flines[2:start_index-1]))    
+            
+            if ts_node(en_list)[0] != 0.0:
 
-with open('ensemble_TS.xyz', 'r') as f:
-    flines = f.read().splitlines()
+                try:
+                    pybel_product = list(pb.readfile(format='xyz', filename='file_xyz.xyz'))[-1]
+                    TS_index = en_list.index(ts_node(en_list)[1])*nlines + 2
+                except:
+                    pybel_product = list(pb.readfile(format='xyz', filename=file))[-1]
+                    TS_index = en_list.index(ts_node(en_list)[1])*nlines
 
-    natoms = int(flines[0])
-    nlines = natoms + 2
+                rdkit_product = pybel_mol_to_rdkit_mol(pybel_product)
+                torsion_deg = rdMolTransforms.GetDihedralDeg(rdkit_product.GetConformer(), 56, 55, 79, 78)
+                if torsion_deg >= 0:
+                    stereochem = 'S'
+                else:
+                    stereochem = 'R'
+                # 
+                idx = re.search(r"pystring_(\d+)", file).groups()[0]
+                
+                infofile.write(f"  Conformer_{idx}     {ts_node(en_list)[2]:.6f}      {stereochem}           {torsion_deg:.3f}\n")
 
-    os.chdir(new_path)
+                ensemble_TS.write('\n'.join(flines[TS_index:TS_index+nlines]))
+                ensemble_TS.write('\n')
 
-    for i, n in enumerate(range(0, len(flines), nlines)):
-        with open('qstart2.inp', 'r') as f_1, open(f'q{i:03d}.TS.inp', 'w') as f_2:
-            for lines in f_1:
-                f_2.write(lines)
-        with open(f'q{i:03d}.TS.inp', 'a+') as f_2:
-            f_2.write('\n'.join(flines[n+2:n+nlines]))
-            f_2.write('\n')
-        with open(f'q{i:03d}.TS.inp', 'a+') as f_2, open('qend2.inp', 'r') as f_3:
-            for lines in f_3:
-                f_2.write(lines)
-
-
-
+    ensemble_TS.close()
+    try:
+        os.remove('file_xyz.xyz')
+    except:
+        pass
         
+    ## Script for submitting the TS jobs
+    ## subprocess.run(['sbatch', f'--array=0-{len(file_list)-1}', './tests/ts_job_array.py'])
 
+    ## Script for making the TS job input files
+
+    new_path = Path('OptTS')
+
+    with open('ensemble_TS.xyz', 'r') as f:
+        flines = f.read().splitlines()
+
+        natoms = int(flines[0])
+        nlines = natoms + 2
+
+        os.chdir(new_path)
+
+        for i, n in enumerate(range(0, len(flines), nlines)):
+            with open('qstart2.inp', 'r') as f_1, open(f'q{i:03d}.TS.inp', 'w') as f_2:
+                for lines in f_1:
+                    f_2.write(lines)
+            with open(f'q{i:03d}.TS.inp', 'a+') as f_2:
+                f_2.write('\n'.join(flines[n+2:n+nlines]))
+                f_2.write('\n')
+            with open(f'q{i:03d}.TS.inp', 'a+') as f_2, open('qend2.inp', 'r') as f_3:
+                for lines in f_3:
+                    f_2.write(lines)
+
+if __name__ == '__main__':
+    analyze()
 
--- src/conformational_sampling/visualization.py
+++ src/conformational_sampling/visualization.py
@@ -34,7 +34,8 @@ import openbabel as ob
 from openbabel import pybel as pb
 import nglview
 
-from conformational_sampling.analyze import ts
+from pygsm.utilities.units import KCAL_MOL_PER_AU
+from conformational_sampling.analyze import ts_node
 
     
 # setup_mol()
@@ -46,13 +47,11 @@ class Conformer:
         string_nodes = list(pb.readfile('xyz', str(self.string_path)))
         self.string_nodes = [MolFromMolBlock(node.write('mol'), removeHs=False)
                              for node in string_nodes] # convert to rdkit
-        self.string_energies = [MolToMolBlock(node).split()[0] for node in self.string_nodes]
-        self.ts_energy = max(self.string_energies)
-        return string_nodes
-        
-        self.ts_node_num
-        self.ts_energy
-        self.ts_rdkit_mol
+        self.string_energies = [float(MolToMolBlock(node).split()[0]) * KCAL_MOL_PER_AU
+                                for node in self.string_nodes]
+        max_diff, self.ts_energy, activation_barrier = ts_node(self.string_energies)
+        self.ts_node_num = self.string_energies.index(self.ts_energy)
+        self.ts_rdkit_mol = self.string_nodes[self.ts_node_num]
 
 class ConformationalSamplingDashboard(param.Parameterized):
 
@@ -68,36 +67,34 @@ class ConformationalSamplingDashboard(param.Parameterized):
     def setup_mols(self):
         # extract the conformers for a molecule from an xyz file
         
-        mol_path = Path('/export/zimmerman/soumikd/py-conformational-sampling/example_l8/')
+        mol_path = Path('/export/zimmerman/soumikd/py-conformational-sampling/')
         string_paths = tuple(mol_path.glob('scratch/pystring_*/opt_converged_000.xyz'))
-        self.mol_confs = []
+        self.mol_confs = {
             # get the conformer index for this string
             int(re.search(r"pystring_(\d+)", str(string_path)).groups()[0]):
-            setup_mol(string_path)
+            Conformer(string_path)
             for string_path in string_paths
+        }
         
-        self.mols = {'ligand_l8': self.mol_confs} # molecule name -> conformer index -> rdkit Mol
+        self.mols = {'ligand_l8': self.mol_confs} # molecule name -> conformer index -> Conformer
         
-        self
-    
     @param.depends('setup_mols', watch=True)
     def dataframe(self):
-        def mol_dataframe(mol_confs):
-            energies = [MolToMolBlock(conf).split()[0] for conf in mol_confs]
-            energies = pd.Series(energies, name='energies (kcal/mol)')
-            energies = pd.to_numeric(energies, errors='coerce')
-            energies = energies.where(energies <= 100) * 627.5 #hartree -> kcal/mol
-            return energies
-        
-        df = pd.concat({name : mol_dataframe(mol_confs) for (name, mol_confs) in self.mols.items()},
-                       names=['mol_name', 'idx'])
-        self.df = df.reset_index()
+        conformer_rows = []
+        for mol_name, mol_confs in self.mols.items():
+            for conf_idx, conformer in mol_confs.items():
+                conformer_rows.append({
+                    'mol_name': mol_name,
+                    'conf_idx': conf_idx,
+                    'ts energy (kcal/mol)': conformer.ts_energy,
+                })
+        self.df = pd.DataFrame(conformer_rows)
     
     @param.depends('dataframe')
     def scatter_plot(self):
         df = self.df
-        plot = df.hvplot.box(by='mol_name', y='energies (kcal/mol)', c='orange', title='Conformer Energies', height=800, width=400, legend=False) 
-        plot *= df.hvplot.scatter(y='energies (kcal/mol)', x='mol_name', c='blue').opts(jitter=0.5)
+        plot = df.hvplot.box(by='mol_name', y='ts energy (kcal/mol)', c='orange', title='Conformer Energies', height=800, width=400, legend=False) 
+        plot *= df.hvplot.scatter(y='ts energy (kcal/mol)', x='mol_name', c='blue').opts(jitter=0.5)
         plot.opts(
             opts.Scatter(tools=['tap', 'hover'], active_tools=['wheel_zoom'],
                         # width=600, height=600,
@@ -110,18 +107,19 @@ class ConformationalSamplingDashboard(param.Parameterized):
     
     param.depends('display_mol', 'dataframe', 'scatter_plot', 'index_conf')
     def app(self):
-        return pn.Row(pn.Column(self.param.refresh, self.scatter_plot), self.display_mol)
+        return pn.Row(pn.Column(self.param.refresh, self.scatter_plot, self.index_conf), self.display_mol)
     
     
     @param.depends('stream.index', 'scatter_plot', watch=True)
     def display_mol(self):
         index = self.stream.index
         if not index: # abort if nothing is selected
-            return None
+            index = [0]
+            # return None
         index = index[0]
         mol_name = self.df.iloc[index]['mol_name']
-        conf_index = int(self.df.iloc[index]['idx'])
-        pdb_block = MolToPDBBlock(self.mols[mol_name][conf_index])
+        conf_index = int(self.df.iloc[index]['conf_idx'])
+        pdb_block = MolToPDBBlock(self.mols[mol_name][conf_index].ts_rdkit_mol)
         viewer = NGLViewer(object=pdb_block, extension='pdb', background="#F7F7F7", min_height=800, sizing_mode="stretch_both")
         return viewer
 
