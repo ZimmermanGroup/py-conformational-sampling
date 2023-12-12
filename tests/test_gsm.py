22e0b294a4ab0182416fc823dcf160abf2da3567
joshkamm <joshkamm@umich.edu>
2023-12-12 00:26:21 -0500

    wip #45 working on tests

--- tests/test_gsm.py
+++ tests/test_gsm.py
@@ -1,36 +1,18 @@
 from pathlib import Path
-import numpy as np
 
 import pytest
 import stk
-import stko
 from xtb.ase.calculator import XTB
 import os
 
-from conformational_sampling.ase_stko_optimizer import ASE
 from conformational_sampling.config import Config
 from conformational_sampling.gsm import stk_gsm
-from conformational_sampling.main import (ConformerEnsembleOptimizer, bind_ligands, bind_to_dimethyl_Pd, load_stk_mol, stk_list_to_xyz_file)
+from conformational_sampling.main import (ConformerEnsembleOptimizer, bind_ligands, load_stk_mol, stk_list_to_xyz_file)
 from conformational_sampling.utils import stk_metal
 
 
-@pytest.mark.xfail(raises=ValueError)
+@pytest.mark.skip(reason='not done implementing')
 def test_gsm():
-    # create dppe bound complex as test stk molecule
-    ligand_path = Path('examples/dppe/ligand.xyz') # name of file containing ligand geometry
-    stk_ligand = load_stk_mol(ligand_path)
-
-    # specifies atoms of the ligand that bind to the metal, in this case as a smarts string
-    functional_group_factory = stk.SmartsFunctionalGroupFactory(
-        smarts='P',
-        bonders=(0,),
-        deleters=(),
-    )
-    stk_ligand = stk.BuildingBlock.init_from_molecule(stk_ligand, functional_groups=[functional_group_factory])
-    stk_mol = bind_to_dimethyl_Pd(stk_ligand)
-    optimizer_sequence = stko.OptimizerSequence(stk.MCHammer(), stko.MetalOptimizer(), ASE(XTB()))
-    stk_mol = optimizer_sequence.optimize(stk_mol)
-    
     # run gsm to eliminate ethane
     # driving coordinates are 1-indexed
     driving_coordinates = [['BREAK',1,54],['BREAK',1,58],['ADD',54,58]]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
+from pathlib import Path
 import numpy as np
+import pytest
 import stk
+from conformational_sampling.ase_stko_optimizer import ASE
+from conformational_sampling.main import bind_to_dimethyl_Pd, load_stk_mol
+import stko
+from xtb.ase.calculator import XTB
 from conformational_sampling.utils import stk_mol_to_pybel_mol
 from conformational_sampling.utils import pybel_mol_to_stk_mol
 
@@ -13,4 +19,39 @@ def test_stk_to_pybel_loop():
     initial_positions = butane_initial.get_position_matrix()
     final_positions = butane_final.get_position_matrix()
     assert np.allclose(initial_positions, final_positions, atol=1e-3)
+
+@pytest.mark.xfail(raises=ValueError) # currently having an issue
+def test_monodentate_optimization():
+    # create dppe bound complex as test stk molecule
+    ligand_path = Path('examples/suzuki/example2_L1.xyz') # name of file containing ligand geometry
+    stk_ligand = load_stk_mol(ligand_path)
+
+    # specifies atoms of the ligand that bind to the metal, in this case as a smarts string
+    functional_group_factory = stk.SmartsFunctionalGroupFactory(
+        smarts='P',
+        bonders=(0,),
+        deleters=(),
+    )
+    stk_ligand = stk.BuildingBlock.init_from_molecule(stk_ligand, functional_groups=[functional_group_factory])
+    stk_mol = bind_to_dimethyl_Pd(stk_ligand)
+    optimizer_sequence = stko.OptimizerSequence(stk.MCHammer(), stko.MetalOptimizer(), ASE(XTB()))
+    stk_mol = optimizer_sequence.optimize(stk_mol)
+    
+
+@pytest.mark.xfail(raises=ValueError) # currently having an issue
+def test_bidentate_optimization():
+    # create dppe bound complex as test stk molecule
+    ligand_path = Path('examples/dppe/ligand.xyz') # name of file containing ligand geometry
+    stk_ligand = load_stk_mol(ligand_path)
+
+    # specifies atoms of the ligand that bind to the metal, in this case as a smarts string
+    functional_group_factory = stk.SmartsFunctionalGroupFactory(
+        smarts='P',
+        bonders=(0,),
+        deleters=(),
+    )
+    stk_ligand = stk.BuildingBlock.init_from_molecule(stk_ligand, functional_groups=[functional_group_factory])
+    stk_mol = bind_to_dimethyl_Pd(stk_ligand)
+    optimizer_sequence = stko.OptimizerSequence(stk.MCHammer(), stko.MetalOptimizer(), ASE(XTB()))
+    stk_mol = optimizer_sequence.optimize(stk_mol)
                                         
