d968977179cd66153135cbf0e695089a22c094c5
joshkamm <joshkamm@umich.edu>
2024-02-09 02:33:32 -0500

    wip #54 working on pytest calling suzuki.py

--- examples/suzuki/suzuki.py
+++ examples/suzuki/suzuki.py
@@ -10,69 +10,73 @@ import os
 from conformational_sampling.ase_stko_optimizer import ASE
 from conformational_sampling.config import Config
 from conformational_sampling.gsm import stk_gsm
-from conformational_sampling.main import (ConformerEnsembleOptimizer, bind_ligands, bind_to_dimethyl_Pd, load_stk_mol, stk_list_to_xyz_file)
+from conformational_sampling.main import (ConformerEnsembleOptimizer, bind_ligands, bind_to_dimethyl_Pd, load_stk_mol, stk_list_to_xyz_file, suzuki_ligand_conf_gen)
 from conformational_sampling.utils import stk_metal
 
 
-def suzuki():
-    stk_ligand_5a = load_stk_mol(Path('example2_5a.xyz'))
-    stk_ligand_6a = load_stk_mol(Path('example2_6a.xyz'))
-    stk_ancillary_ligand = load_stk_mol(Path('example2_L1.xyz'))
+# def suzuki():
+stk_ligand_5a = load_stk_mol(Path('example2_5a.xyz'))
+stk_ligand_6a = load_stk_mol(Path('example2_6a.xyz'))
+stk_ancillary_ligand = load_stk_mol(Path('example2_L1.xyz'))
 
-    # specify atoms of each ligand that bind to the metal, in this case as a smarts string
-    functional_group_factory = stk.SmartsFunctionalGroupFactory(
-        smarts='cBr',
-        bonders=(0,),
-        deleters=(1,),
-    )
-    stk_ligand_5a = stk.BuildingBlock.init_from_molecule(stk_ligand_5a, functional_groups=[functional_group_factory])
-    functional_group_factory = stk.SmartsFunctionalGroupFactory(
-        smarts='cB([O][H])[O][H]',
-        bonders=(0,),
-        deleters=tuple(range(1,6)),
-    )
-    stk_ligand_6a = stk.BuildingBlock.init_from_molecule(stk_ligand_6a, functional_groups=[functional_group_factory])
-    functional_group_factory = stk.SmartsFunctionalGroupFactory(
-        smarts='P',
-        bonders=(0,),
-        deleters=(),
-    )
-    stk_ancillary_ligand = stk.BuildingBlock.init_from_molecule(stk_ancillary_ligand, functional_groups=[functional_group_factory])
-    stk_mol = bind_ligands(stk_metal('Pd'), stk_ancillary_ligand, stk_ligand_5a, stk_ligand_6a)
-    stk_list_to_xyz_file([stk_mol], 'test_Pd_complex.xyz')
-    
-    config = Config(
-        xtb_path='/export/apps/CentOS7/xtb/xtb/bin/xtb',
-        #ase_calculator=XTB(),
-        max_dft_opt_steps=30,
-        # num_cpus=16,
-        dft_cpus_per_opt=16,
-    )
+# specify functional groups of each ligand that bind to the metal, 
+# in this case as a smarts string
+functional_group_factory = stk.SmartsFunctionalGroupFactory(
+    smarts='cBr',
+    bonders=(0,),
+    deleters=(1,),
+)
+stk_ligand_5a = stk.BuildingBlock.init_from_molecule(stk_ligand_5a, functional_groups=[functional_group_factory])
+functional_group_factory = stk.SmartsFunctionalGroupFactory(
+    smarts='cB([O][H])[O][H]',
+    bonders=(0,),
+    deleters=tuple(range(1,6)),
+)
+stk_ligand_6a = stk.BuildingBlock.init_from_molecule(stk_ligand_6a, functional_groups=[functional_group_factory])
+functional_group_factory = stk.SmartsFunctionalGroupFactory(
+    smarts='P',
+    bonders=(0,),
+    deleters=(),
+)
+stk_ancillary_ligand = stk.BuildingBlock.init_from_molecule(stk_ancillary_ligand, functional_groups=[functional_group_factory])
+# stk_mol = bind_ligands(stk_metal('Pd'), stk_ancillary_ligand, stk_ligand_5a, stk_ligand_6a)
 
-    from ase.calculators.qchem import QChem
-    os.environ['QCSCRATCH'] = os.environ['SLURM_LOCAL_SCRATCH']
-    config.ase_calculator = QChem(
-        method='PBE',
-        # basis='6-31G',
-        #basis='STO-3G',
-        basis='LANL2DZ',
-        ecp='fit-LANL2DZ',
-        SCF_CONVERGENCE='5',
-        SCF_MAX_CYCLES='300',
-        SCF_ALGORITHM='DIIS',
-        nt=config.dft_cpus_per_opt,
-    )
+# stk_list_to_xyz_file([stk_mol], 'test_Pd_complex.xyz')
 
-    conformer_ensemble_optimizer = ConformerEnsembleOptimizer([stk_mol], config)
-    optimized_stk_mol = conformer_ensemble_optimizer.optimize()[0]
+config = Config(
+    # xtb_path='/export/apps/CentOS7/xtb/xtb/bin/xtb',
+    #ase_calculator=XTB(),
+    # max_dft_opt_steps=30,
+    initial_conformers=2,
+    num_cpus=2,
+    # dft_cpus_per_opt=16,
+)
+suzuki_ligand_conf_gen(stk_ligand_5a, stk_ligand_6a, stk_ancillary_ligand, config)
 
-    driving_coordinates = [('ADD',56,80),('BREAK',1,56),('BREAK',1,80)]
-    stk_gsm(
-        stk_mol=optimized_stk_mol,
-        driving_coordinates=driving_coordinates,
-        config=config,
-    )
-    assert True
+# from ase.calculators.qchem import QChem
+# os.environ['QCSCRATCH'] = os.environ['SLURM_LOCAL_SCRATCH']
+# config.ase_calculator = QChem(
+#     method='PBE',
+#     # basis='6-31G',
+#     #basis='STO-3G',
+#     basis='LANL2DZ',
+#     ecp='fit-LANL2DZ',
+#     SCF_CONVERGENCE='5',
+#     SCF_MAX_CYCLES='300',
+#     SCF_ALGORITHM='DIIS',
+#     nt=config.dft_cpus_per_opt,
+# )
 
-if __name__ == "__main__":
-    suzuki()
+# conformer_ensemble_optimizer = ConformerEnsembleOptimizer([stk_mol], config)
+# optimized_stk_mol = conformer_ensemble_optimizer.optimize()[0]
+
+driving_coordinates = [('ADD',56,80),('BREAK',1,56),('BREAK',1,80)]
+# stk_gsm(
+#     stk_mol=optimized_stk_mol,
+#     driving_coordinates=driving_coordinates,
+#     config=config,
+# )
+assert True
+
+# if __name__ == "__main__":
+#     suzuki()
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,3 +6,9 @@ def test_examples():
     assert True
     example_vars = runpy.run_path(Path(__file__).parents[1] / 'examples' / 'gsm' / 'simple_gsm.py')
     assert example_vars['hello'] == 'hello world'
+
+
+def test_suzuki_example():
+    # CHANGE DIRECTORY INTO THE SUZUKI EXAMPLE BEFORE EXECUTING THE SCRIPT
+    example_vars = runpy.run_path(Path(__file__).parents[1] / 'examples' / 'suzuki' / 'suzuki.py')
+    
