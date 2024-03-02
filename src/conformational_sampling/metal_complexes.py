fe73aaf7aaecae1855de6c4f9a583c1e8768530a
Joshua Kammeraad <joshkamm@users.noreply.github.com>
2024-03-02 22:52:00 +0000

    wip #56 work on test
    also refactor CatalyticReactionComplex to own file

--- src/conformational_sampling/metal_complexes.py
+++ src/conformational_sampling/metal_complexes.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass
 from math import cos, pi, sin
 
-import stk
 from stk.molecular.topology_graphs import Edge
 from stk.molecular.topology_graphs.metal_complex import MetalComplex
 from stk.molecular.topology_graphs.metal_complex.vertices import (
@@ -9,9 +7,6 @@ from stk.molecular.topology_graphs.metal_complex.vertices import (
     MetalVertex,
     MonoDentateLigandVertex,
 )
-
-from conformational_sampling.main import bind_ligands
-from conformational_sampling.utils import stk_metal
 
 
 class TwoMonoOneBidentateSquarePlanar(MetalComplex):
@@ -86,54 +81,3 @@ class OneLargeTwoSmallMonodentateTrigonalPlanar(MetalComplex):
             position=(cos(4*pi/3), sin(4*pi/3), 0),
         ),
     )
-
-
-@dataclass
-class CatalyticReactionComplex:
-    metal: stk.Molecule
-    ancillary_ligand: stk.Molecule
-    reactive_ligand_1: stk.Molecule
-    reactive_ligand_2: stk.Molecule
-    
-    
-    def __post_init__(self) -> None:
-        'create a complex with ane ancillary ligand and two reactive ligands, like bind_ligands'
-        self.complex = bind_ligands(
-            self.metal,
-            self.ancillary_ligand,
-            self.reactive_ligand_1,
-            self.reactive_ligand_2,
-        )
-    
-    
-    def gen_reductive_elim_drive_coords(self):
-        'generate reductive elimination driving coordinates for this complex'
-        atom_infos = list(self.complex.get_atom_infos())
-        driving_coordinates = []
-
-        def is_metal_reactive_ligand_bond(
-            atom_info_1: stk.AtomInfo, atom_info_2: stk.AtomInfo
-        ) -> bool:
-            building_block_1 = atom_info_1.get_building_block()
-            building_block_2 = atom_info_2.get_building_block()
-            return building_block_1 is self.metal and building_block_2 in (
-                self.reactive_ligand_1,
-                self.reactive_ligand_2,
-            )
-
-        # determine driving coordinates to break
-        for bond_info in self.complex.get_bond_infos():
-            if bond_info.get_building_block() is None:
-                bond = bond_info.get_bond()
-                atom_id_1, atom_id_2 = bond.get_atom1().get_id(), bond.get_atom2().get_id()
-                atom_info_1 = next(self.complex.get_atom_infos(atom_id_1))
-                atom_info_2 = next(self.complex.get_atom_infos(atom_id_2))
-                if (is_metal_reactive_ligand_bond(atom_info_1, atom_info_2)
-                    or is_metal_reactive_ligand_bond(atom_info_2, atom_info_1)):
-                    # this bond is broken in reductive elimination
-                    driving_coordinates += ('BREAK', atom_id_1, atom_id_2)
-                
-        # CHECK THAT THERE ARE TWO BREAKING DRIVING COORDS
-
-        # go from zero-indexed to one-indexed
-        return [(type, i + 1, j + 1) for (type, i, j) in driving_coordinates]
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,6 +1,11 @@
-from pathlib import Path
 import runpy
 import shutil
+from pathlib import Path
+
+import stk
+from conformational_sampling.catalytic_reaction_complex import CatalyticReactionComplex
+from conformational_sampling.main import load_stk_mol
+from conformational_sampling.utils import stk_metal
 
 
 def test_suzuki_example(tmp_path, monkeypatch):
@@ -11,4 +16,50 @@ def test_suzuki_example(tmp_path, monkeypatch):
     assert True
     # example of how to read variables from resulting environment
     # assert example_vars['hello'] == 'hello world'
-    
+
+
+def test_reductive_elim_drive_coords(monkeypatch):
+    example_path = Path(__file__).parents[1] / 'examples' / 'suzuki'
+    monkeypatch.chdir(example_path)
+    stk_ligand_5a = load_stk_mol(Path('example2_5a.xyz'))
+    stk_ligand_6a = load_stk_mol(Path('example2_6a.xyz'))
+    stk_ancillary_ligand = load_stk_mol(Path('example2_L1.xyz'))
+
+    # specify functional groups of each ligand that bind to the metal,
+    # in this case as smarts strings
+    functional_group_factory = stk.SmartsFunctionalGroupFactory(
+        smarts='cBr',
+        bonders=(0,),
+        deleters=(1,),
+    )
+    stk_ligand_5a = stk.BuildingBlock.init_from_molecule(
+        stk_ligand_5a, functional_groups=[functional_group_factory]
+    )
+    functional_group_factory = stk.SmartsFunctionalGroupFactory(
+        smarts='cB([O][H])[O][H]',
+        bonders=(0,),
+        deleters=tuple(range(1, 6)),
+    )
+    stk_ligand_6a = stk.BuildingBlock.init_from_molecule(
+        stk_ligand_6a, functional_groups=[functional_group_factory]
+    )
+    functional_group_factory = stk.SmartsFunctionalGroupFactory(
+        smarts='P',
+        bonders=(0,),
+        deleters=(),
+    )
+    stk_ancillary_ligand = stk.BuildingBlock.init_from_molecule(
+        stk_ancillary_ligand, functional_groups=[functional_group_factory]
+    )
+
+    reaction_complex = CatalyticReactionComplex(
+        metal=stk_metal('Pd'),
+        ancillary_ligand=stk_ancillary_ligand,
+        reactive_ligand_1=stk_ligand_5a,
+        reactive_ligand_2=stk_ligand_6a,
+    )
+    assert set(reaction_complex.gen_reductive_elim_drive_coords()) == {
+        ('ADD', 56, 80),
+        ('BREAK', 1, 56),
+        ('BREAK', 1, 80),
+    }
